71cb5ef6b47856eb4ed4440bfde9c6626d8890a3
hellhamster <a.n.baranov@outlook.com>
2022-01-12 20:28:40 +0300

    prime corrected

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -9,7 +9,7 @@ def main():
 
     @template_game
     def prime_logic(answer='', right_answer=''):
-        number = random.randint(1, 100)
+        number = random.randint(2, 100)
         dividers = [b for b in range(2, number // 2) if number % b == 0]
         right_answer = 'no' if dividers else 'yes'
 
