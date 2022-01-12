1408809582c978b1ee54fb513ff87015c0726f80
hellhamster <a.n.baranov@outlook.com>
2022-01-12 15:19:58 +0300

    delete prompt from cli

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,9 @@
 """docstring for cli file."""
-import prompt
 
 
 def welcome_user():
     """Welcome user func. Returns user name"""
-    name = prompt.string('May I have your name? ')
+    name = input('May I have your name?')
     print('Hello,', name, '!')
 
     return name
